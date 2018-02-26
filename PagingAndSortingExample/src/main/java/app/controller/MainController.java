efee0753bb44ecdb31dc6577fd5882d432d7a2ca
jose_valenzuela_soto <leunamesoj.valenzuelasoto@gmail.com>
2018-02-26 18:33:26 -0500

    Adding Routing Constants

--- PagingAndSortingExample/src/main/java/app/controller/MainController.java
+++ PagingAndSortingExample/src/main/java/app/controller/MainController.java
@@ -1,16 +1,17 @@
 package app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import app.model.Alumno;
 import app.service.AlumnoService;
 
 @RestController
@@ -19,19 +20,18 @@ public class MainController {
 	@Autowired
 	AlumnoService alumnoService;
 	
-	@GetMapping("/alumnos/paged")
-	public ResponseEntity<?> getAlumnos(@PageableDefault Pageable p,PagedResourcesAssembler<?> assembler){
+	@GetMapping(RoutingConstants.WS_ALUMONS_EP)
+	public ResponseEntity<?> getAlumnos(@PageableDefault Pageable p,PagedResourcesAssembler<Alumno> assembler){
 		
-		Page alumnos = alumnoService.findAll(p);
-		
-		PagedResources<Resource> pagedRes = assembler.toResource(alumnos);
-		
+		Page<Alumno> alumnos = alumnoService.findAll(p);
 		if(alumnos!=null){
+			PagedResources<Resource<Alumno>> pagedRes = assembler.toResource(alumnos);
 			return ResponseEntity.ok(pagedRes);
 		}else{
 			return ResponseEntity.notFound().build();
 		}
-		
-		//return ResponseEntity.ok(pagedRes);
+
 	}
+	
+	
 }
---
+++ PagingAndSortingExample/src/main/java/app/controller/RoutingConstants.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package app.controller;
+
+/**
+ * @author José Valenzuela Soto.
+ *
+ */
+public interface RoutingConstants {
+
+	public final static String WS_ALUMONS_EP="/alumnos/paged";
+}
